2e22f621de1202529339fdd784b68a76bd586638
renatgrishin <cynvox@gmail.com>
2021-06-17 16:09:28 +0300

    Desktop, Mobile, animation

--- js/main.js
+++ js/main.js
@@ -4,9 +4,51 @@ $(function(){
 		$('.mb-nav-show-h3').attr('id', 'mb-nav-show-h3');
 		$('.mb-nav-show-close').attr('id', 'mb-nav-show-close');
 	});
+
 	$('.mb-nav-show-close').click(()=>{
 		$('.mb-nav').attr('id', 'mb-nav');
 		$('.mb-nav-show-h3').removeAttr('id');
 		$('.mb-nav-show-close').removeAttr('id');
-	})
+	});
+
+	$('.ft-spoiler').click(handler);
 })
+
+function handler(e) {
+	let target = $(e.target);
+	console.log(target)
+
+	function offOn(element) {
+		if(element.text() == '+'){
+			element.text('-')
+		}else{
+			element.text('+')
+		}
+	}
+
+	if ( target.is("article.ft-spoiler")) {
+		target.children('ul').toggleClass('show-block');
+		offOn(target.children('span'));
+	}
+	if ( target.is("h4") ){
+		target.siblings('ul').toggleClass('show-block');
+		offOn(target.siblings('span'));
+	}
+	if ( target.is("span.spoiler-arrow") ){
+		target.siblings('ul').toggleClass('show-block');
+		offOn(target);
+	}
+}
+
+function dtAnim(entry) {
+	console.log(entry);
+	entry.forEach((change) => {
+		if(change.isIntersecting){
+			change.target.classList.add('dt-anim-show');
+		}
+	});
+}
+
+let observer = new IntersectionObserver(dtAnim, {threshold: [0.5]});
+let elements = $('.dt-anim');
+for(let key of elements) observer.observe(key);
